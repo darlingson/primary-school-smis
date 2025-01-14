f346e47df2fdffdca6b9118a8d390747f7693f14
darlingson <dmakuwila@gmail.com>
2025-01-14 07:49:17 +0200

    installed pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxtjs/supabase'],
+  modules: ['@nuxt/ui', '@nuxtjs/supabase', '@pinia/nuxt'],
   supabase: {
     redirect:false
   },
