ade5086b7088d40724d78567c8e9929193f87a85
darlingson <dmakuwila@gmail.com>
2025-01-21 20:49:58 +0200

    added endpoint for adding schools

---
+++ server/admin/add-school.post.ts
@@ -0,0 +1,38 @@
+import { createClient } from '@supabase/supabase-js'
+
+export default defineEventHandler(async (event) => {
+    try {
+        const supabase = createClient(
+            process.env.SUPABASE_URL as string,
+            process.env.SUPABASE_KEY as string
+        )
+        const body = await readBody(event)
+
+        const { data, error } = await supabase
+            .from('schools')
+            .insert([
+                {
+                    name: body.name,
+                    location: body.location,
+                    contact_email: body.contactEmail,
+                    contact_phone: body.contactPhone,
+                    address: body.address
+                }
+            ])
+            .select()
+
+        if (error) {
+            throw error
+        }
+
+        return {
+            status: 'success',
+            data
+        }
+    } catch (error) {
+        return {
+            status: 'error',
+            error: error.message
+        }
+    }
+})
